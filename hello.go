86a53769577b72693c5182e3467cff02d8df820e
sandeep-suthar01 <47204542+sandeep-suthar01@users.noreply.github.com>
2019-01-31 15:32:26 +0530

    Update hello.go

--- hello.go
+++ hello.go
@@ -1,14 +1,30 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+  "fmt"
+  "log"
+  "net/http"
+  "os"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested:", r.URL.Path)
-	})
+func handler(w http.ResponseWriter, r *http.Request) {
+  log.Print("Hello world received a request.")
+  target := os.Getenv("TARGET")
+  if target == "" {
+    target = "World"
+  }
+  fmt.Fprintf(w, "Hello %s!\n", target)
+}
 
-	http.ListenAndServe(":8080", nil)
+func main() {
+  log.Print("Hello world sample started.")
+
+  http.HandleFunc("/", handler)
+
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8080"
+  }
+
+  log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
